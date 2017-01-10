4a63bda0b01e85ba76190c8ce57738047afa4abb
PatchDynamics <xone@fromthegut.org>
2017-01-09 18:05:49 -0800

    'Imputing code.  Uses the 'nearest days' approach, and has an option to impute by mean or linear model

---
+++ example.R
@@ -0,0 +1,38 @@
+library(dataRetrieval)
+library(waterdata)
+
+var='SRP..mg.L..as.P'
+date.var = 'Datetime..date.and.time.of.sample.collection.'
+
+data <- read.csv(file="Maumeedata.csv", header=T)
+ind  = which(data[,c(var)]<=0)
+data = data[-ind,c(var, date.var)]
+names(data) = c('var', 'date.var')
+
+# this USGS code is not working for loading maumee currently
+# usgs.station.id = "04193500"
+# sdate = "1970-01-01"; edate = "2016-01-01"
+# discharge = importDVs(usgs.station.id,code="00060", sdate=sdate, edate=edate)
+
+
+discharge = maumee # because usgs is being annoying
+ignore = seq.POSIXt(as.POSIXlt('1978-10-01', tz=Sys.timezone()), as.POSIXlt('1981-10-12', tz=Sys.timezone()),'days')
+
+monthly.loads.SRP = impute.daily.concentrations(data$var, data$date.var, discharge$val, discharge$datetime, ignore)
+write.csv(monthly.loads.SRP, file='monthly.loads.SRP.csv')
+
+# TP
+var='TP..mg.L.as.P'
+date.var = 'Datetime..date.and.time.of.sample.collection.'
+
+data <- read.csv(file="Maumeedata.csv", header=T)
+ind  = which(data[,c(var)]<=0)
+data = data[-ind,c(var, date.var)]
+names(data) = c('var', 'date.var')
+ignore = seq.POSIXt(as.POSIXlt('1978-10-01', tz=Sys.timezone()), as.POSIXlt('1981-10-12', tz=Sys.timezone()),'days')
+
+monthly.loads.TP = impute.daily.concentrations(data$var, data$date.var, discharge$val, discharge$datetime, ignore, 
+                                            use.linear.model = TRUE)
+write.csv(monthly.loads.TP, file='monthly.loads.TP.csv')
+
+
---
+++ impute.concentrations.R
@@ -0,0 +1,61 @@
+library(xts)
+library(dplyr)
+
+impute.daily.concentrations = function(concentration, concentration.dates, 
+                                       discharge, discharge.dates, 
+                                       ignore=c(),
+                                       use.linear.model=FALSE,
+                                       window.size=10 #,
+                                       #mode='nearest'  # option not implemented ('rolling' mean)
+){
+  data = data.frame(var = concentration, date.var=concentration.dates)
+  discharge = data.frame(val=discharge, datetime=discharge.dates)
+  
+  data$date= as.Date(data[,c('date.var')], format='%m/%d/%Y')
+  data.days = aggregate(as.formula('var ~ date'),
+                        data, mean)
+  
+  discharge.xts = xts(discharge$val, order.by=as.POSIXlt(as.character(discharge$datetime), 
+                                                         format='%Y-%m-%d', tz=Sys.timezone()))
+  
+  discharge.xts = discharge.xts['1975-01-01/']
+  
+  data.xts = xts(data.days$var, order.by=as.POSIXlt(as.character(data.days$date), format='%Y-%m-%d', tz=Sys.timezone()))
+  data.xts = merge(data.xts, discharge.xts)
+  names(data.xts) = c('var', 'discharge')
+  
+  data.xts$imputed = NA
+  data.xts$skip = FALSE
+  data.xts$skip[ignore] = TRUE
+  impute = which(is.na(data.xts$var & data.xts$skip==FALSE))
+  nas = which(is.na(data.xts$var))
+  for(i in nas){
+    d = abs(.index(data.xts)[i] - .index(data.xts))
+    d[nas] = NA
+    df.sort = data.frame(dist = d, var = data.xts$var, discharge=data.xts$discharge)
+    df.sort = dplyr::arrange(df.sort, dist)
+    if(use.linear.model){
+      lm.model = lm(var ~ discharge, df.sort[1:window.size,])
+      imputed.conf = predict(lm.model, data.frame(discharge = data.xts$discharge[i]))
+    } else {
+      imputed.conc = mean(df.sort[1:window.size,]$var)
+    }
+    data.xts$imputed[i] = imputed.conc
+  }
+  
+  data.xts$combined = data.xts$var
+  data.xts$combined[is.na(data.xts$combined)] = data.xts$imputed[is.na(data.xts$combined)]
+  
+  data.xts$daily.loads = data.xts$discharge * data.xts$combined * 28.31685 * 60 * 60 * 24
+  data.xts$month = .indexmon(data.xts)
+  data.xts$year = .indexyear(data.xts)
+  data.df = as.data.frame(data.xts)
+  monthly.loads = aggregate(list(monthly.loads = data.df$daily.loads),
+                            list(month=data.df$month, year=data.df$year)
+                            , sum)
+  monthly.loads$monthly.loads = monthly.loads$monthly.loads * 1e-9
+  return(monthly.loads)
+}
+
+
+
